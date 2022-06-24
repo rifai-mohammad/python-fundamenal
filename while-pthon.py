f1fc1a46c5feb32324e45a9b9e56c5ac0611be2f
rifais <rifaisuwarto@gmail.com>
2022-06-25 04:12:27 +0700

    perulangan dengan while dengan kondisi tertentu

--- while-pthon.py
+++ while-pthon.py
@@ -1,12 +1,19 @@
 print('ibu menyuruh membaca buku')
 jumlah_buku = 10
 
-jumlah_buku_sudah_dibaca = 0
+jumlah_paham = 0
+jumlah_baca = 0
 
-print(f'jumlah buku suda dibaca {jumlah_buku_sudah_dibaca}')
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f'buku ke {jumlah_paham + 1} belum dipahami ')
+    else:
+        jumlah_paham = jumlah_paham + 1
+        print(f'buku ke {jumlah_paham} sudah dipahami')
 
-while jumlah_buku_sudah_dibaca < jumlah_buku:
-    jumlah_buku_sudah_dibaca = jumlah_buku_sudah_dibaca + 1
-    print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
-
-print(f'jumlah buku yang selesai dibaca {jumlah_buku_sudah_dibaca}')
+print(f'jumlah buku yang selesai dibaca dan dipahami {jumlah_paham}')
+if jumlah_paham == jumlah_buku:
+    print('Semua buku bisa dipahami')
+else:
+    print('Tidak semua buku bisa dipahami')
